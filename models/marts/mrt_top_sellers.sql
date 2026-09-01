82ae859e6daae1ac2c04016ce9b360578bb70583
mikolajaksamit <157887790+mikolajaksamit@users.noreply.github.com>
2026-09-01 16:01:31 +0200

    Fix mrt_top_sellers grain

--- models/marts/mrt_top_sellers.sql
+++ models/marts/mrt_top_sellers.sql
@@ -1,14 +1,11 @@
 {{ config(materialized='table') }}
-
 select
     s.seller_id,
     s.seller_city,
     s.seller_state,
-    DATE(o.order_purchase_timestamp) as order_date,
     sum(oi.price) as total_revenue,
     count(oi.order_id) as items_sold
 from {{ ref('stg_order_items') }} oi
 join {{ ref('stg_sellers') }} s on oi.seller_id = s.seller_id
-left join {{ref('stg_orders')}} o on o.order_id = oi.order_id
-group by 1, 2, 3, 4
+group by 1, 2, 3
 order by total_revenue desc
